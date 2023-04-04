7aa9d22a062ec8e210ab761adee5439733d5f8c9
Amethyst <1066840101@qq.com>
2023-04-04 19:53:26 +0800

    -- optimize  - use reflect replace switch

--- coolq/job.go
+++ coolq/job.go
@@ -3,7 +3,7 @@ package coolq
 import (
 	"github.com/Mrs4s/go-cqhttp/util/cron"
 	"github.com/Mrs4s/go-cqhttp/util/top_list"
-	log "github.com/sirupsen/logrus"
+	"reflect"
 )
 
 const (
@@ -34,15 +34,9 @@ func (r *ReportJob) JobType() int {
 }
 
 func (bot *CQBot) RegisterJob(job IJob) {
-	switch job.JobType() {
-	case Report:
-		reportJob := job.(*ReportJob)
-		reportJob.RunJob()
-		JobModels[reportJob.Model] = reportJob.Corn
-		log.Infof("注册定时推送任务：%s", reportJob.Model)
-	default:
-
-	}
+	reflect.ValueOf(job).
+		MethodByName("RunJob").
+		Call(nil)
 }
 
 func (bot *CQBot) WeiboHotReporter(group int64, corn string) *ReportJob {
